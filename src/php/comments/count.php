b882249c07ee063178222b6b84c44ed2a92f0808
Steve Laskaridis <stevelaskaridis@gmail.com>
2016-12-28 17:36:57 +0200

    Add comments count page

---
+++ src/php/comments/count.php
@@ -0,0 +1,9 @@
+<?php
+  include("../connect.php");
+
+  $statement = $pdo->prepare(
+      "SELECT count(*) as comments_count FROM comments");
+  $statement->execute();
+  $results = $statement->fetchAll();
+  echo json_encode($results);
+?>
