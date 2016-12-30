c64ff731cf8f05f95dec8afaae538f0fb91c7acb
Steve Laskaridis <stevelaskaridis@gmail.com>
2016-12-30 12:50:59 +0200

    Fix list comments without offset and limit

--- src/php/comments/list.php
+++ src/php/comments/list.php
@@ -4,10 +4,16 @@
     $limit = $_GET['limit'];
     $offset = $_GET['offset'];
 
-    $statement = $pdo->prepare(
-        "SELECT * FROM comments ORDER BY id DESC LIMIT :limit OFFSET :offset");
-    $statement->bindParam(":limit", $limit);
-    $statement->bindParam(":offset", $offset);
+    if (isset($_GET['limit']) && isset($_GET['offset'])) {
+      $statement = $pdo->prepare(
+          "SELECT * FROM comments ORDER BY id DESC LIMIT :limit OFFSET :offset");
+      $statement->bindParam(":limit", $limit);
+      $statement->bindParam(":offset", $offset);
+    }
+    else {
+      $statement = $pdo->prepare(
+          "SELECT * FROM comments ORDER BY id DESC");
+    }
     $statement->execute();
     $results = $statement->fetchAll();
     echo json_encode($results);
